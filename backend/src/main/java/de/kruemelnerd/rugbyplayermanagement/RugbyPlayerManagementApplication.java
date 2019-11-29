6fa8fbe6369f845c7b9974ee97c91d71e70df274
Philipp Veit <hallo@philippveit.de>
2019-11-29 16:29:56 +0100

    init Backend

---
+++ backend/src/main/java/de/kruemelnerd/rugbyplayermanagement/RugbyPlayerManagementApplication.java
@@ -0,0 +1,13 @@
+package de.kruemelnerd.rugbyplayermanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RugbyPlayerManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RugbyPlayerManagementApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/de/kruemelnerd/rugbyplayermanagement/RugbyPlayerManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package de.kruemelnerd.rugbyplayermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RugbyPlayerManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
