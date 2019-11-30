b2949b448d73d6277095a26d8b5a2ac2bfdacd22
Philipp Veit <hallo@philippveit.de>
2019-11-30 17:03:02 +0100

    init frontend

---
+++ frontend/src/test/counter.spec.js
@@ -0,0 +1,14 @@
+import {mount} from '@vue/test-utils'
+import Counter from '../components/counter'
+
+
+// Now mount the component and you have the wrapper
+const wrapper = mount(Counter)
+
+// You can access the actual Vue instance via `wrapper.vm`
+const vm = wrapper.vm
+
+// To inspect the wrapper deeper just log it to the console
+// and your adventure with the Vue Test Utils begins
+console.log(wrapper)
+
